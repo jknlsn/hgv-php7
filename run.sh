bf5fd94d52a01d6887c42119635254bf6f9bf7cf
Jake Nelson <j.n@live.com.au>
2016-01-04 10:48:37 +0800

    Testing shell script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash         
+
+echo "Installing Updates and Software"
+
+echo "- apt-get install software-properties-common python-software-properties"
+sudo apt-get install software-properties-common python-software-properties -y
+
+echo "- add-apt-repository ppa:ondrej/php-7.0 && sudo add-apt-repository ppa:ansible/ansible"
+yes '' | sudo add-apt-repository ppa:ondrej/php-7.0 && sudo add-apt-repository ppa:ansible/ansible
+
+echo "- apt-get update && sudo apt-get upgrade"
+sudo apt-get update && sudo apt-get upgrade
+
+echo "- apt-get install ansible git"
+sudo apt-get install ansible git -y
